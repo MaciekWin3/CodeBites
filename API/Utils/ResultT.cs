010bdf695ec22db2ec4e17c506f3bc29f146ed9c
Maciej Winnik <maciekwin3@gmail.com>
2024-05-02 11:10:19 +0200

    Add Result class

---
+++ API/Utils/ResultT.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Utils
+{
+    public class Result<T> : Result
+    {
+        protected internal Result(T value, bool success, string error)
+            : base(success, error)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+    }
+}
