3333177fbcf4d71da89dccb01666cb789d4a11c3
Maciej Winnik <maciekwin3@gmail.com>
2024-05-03 11:59:46 +0200

    Update base entity model

--- API/Data/BaseEntity.cs
+++ API/Data/BaseEntity.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Repo
 {
@@ -14,10 +13,8 @@ namespace API.Repo
         [MaxLength(255)]
         public string ModifiedBy { get; set; } = string.Empty;
 
-        [Column(TypeName = "datetime2(2)")]
         public DateTime CreatedAt { get; set; }
 
-        [Column(TypeName = "datetime2(2)")]
         public DateTime ModifiedAt { get; set; }
 
         public bool IsDeleted { get; set; }
