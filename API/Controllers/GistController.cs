caaf7db7b0b9d601e6c1b3bebf314018c7ab8609
Maciej Winnik <maciekwin3@gmail.com>
2024-05-05 10:47:00 +0200

    Update controler - add auth and DTOs mappings

--- API/Controllers/GistController.cs
+++ API/Controllers/GistController.cs
@@ -1,23 +1,30 @@
-﻿using API.Models;
+﻿using API.DTOs;
+using API.Models;
 using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using System.Net;
 
 namespace API.Controllers
 {
+    [Authorize]
     [ApiController]
     [ApiExplorerSettings(GroupName = "Gists")]
     [Route("/api/gists")]
     public class GistController : Controller
     {
         private readonly IGistService gistService;
-        public GistController(IGistService gistService)
+        private readonly UserManager<IdentityUser> userManager;
+        public GistController(IGistService gistService, UserManager<IdentityUser> userManager)
         {
             this.gistService = gistService;
+            this.userManager = userManager;
         }
 
         [HttpGet]
+        [AllowAnonymous]
         [SwaggerResponse((int)HttpStatusCode.OK, "Public gists returned successfully", typeof(List<Gist>))]
         [SwaggerOperation(Summary = "Fetches all public gists")]
         public async Task<IActionResult> GetPublicGistsAsync()
@@ -48,19 +55,35 @@ namespace API.Controllers
         [HttpPost]
         [SwaggerResponse((int)HttpStatusCode.Created, "Gist created successfully", typeof(Gist))]
         [SwaggerOperation(Summary = "Creates a new gist")]
-        public async Task<IActionResult> CreateAsync([FromBody] Gist gist)
+        public async Task<IActionResult> CreateAsync([FromBody] GistCreateDTO gistDto)
         {
+            // get me current user
+            var user = await userManager.GetUserAsync(User);
+            var gist = gistDto.ToModel(user!);
             var result = await gistService.CreateAsync(gist);
             return Created("", result.GetValue());
         }
 
-        [HttpPost]
+        [HttpPatch]
         [Route("upvote")]
         [SwaggerResponse((int)HttpStatusCode.OK, "Gist upvoted successfully")]
         [SwaggerOperation(Summary = "Upvotes a gist")]
-        public async Task<IActionResult> UpvoteAsync([FromBody] Gist gist)
+        public async Task<IActionResult> UpvoteAsync([FromBody] GistUpdateDTO gistDto)
         {
+            var gistToUpdate = await gistService.GetByIdAsync(gistDto.Id);
+            var gist = gistDto.ToModel(gistToUpdate.GetValue());
             await gistService.UpvoteAsync(gist);
+            return Ok();
+        }
+
+        [HttpPut]
+        [SwaggerResponse((int)HttpStatusCode.OK, "Gist updated successfully", typeof(Gist))]
+        [SwaggerOperation(Summary = "Updates a gist")]
+        public async Task<IActionResult> UpdateAsync([FromBody] GistUpdateDTO gistDto)
+        {
+            var gistToUpdate = await gistService.GetByIdAsync(gistDto.Id);
+            var gist = gistDto.ToModel(gistToUpdate.GetValue());
+            var result = await gistService.UpdateAsync(gist);
             return Ok();
         }
     }
