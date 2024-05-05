6f962f07ba29201dd58cb4f8aeaab67786867086
Maciej Winnik <maciekwin3@gmail.com>
2024-05-05 10:46:39 +0200

    Add DTOs

---
+++ API/DTOs/GistCreateDTO.cs
@@ -0,0 +1,29 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.DTOs
+{
+    public record GistCreateDTO
+    {
+        public bool Public { get; set; }
+        public required string Title { get; set; }
+        public string? Language { get; set; }
+        public string? Description { get; set; }
+        public required string Content { get; set; }
+
+        public Gist ToModel(IdentityUser owner)
+        {
+            return new Gist
+            {
+                Id = Guid.NewGuid(),
+                Public = Public,
+                Title = Title,
+                Language = Language,
+                Description = Description,
+                Content = Content,
+                Upvotes = 0,
+                Owner = owner
+            };
+        }
+    }
+}
---
+++ API/DTOs/GistUpdateDTO.cs
@@ -0,0 +1,37 @@
+﻿using API.Models;
+
+namespace API.DTOs
+{
+    public class GistUpdateDTO
+    {
+        public Guid Id { get; set; }
+        public bool Public { get; set; }
+        public required string Title { get; set; }
+        public string? Language { get; set; }
+        public string? Description { get; set; }
+        public required string Content { get; set; }
+
+        public Gist ToModel(Gist existingGist)
+        {
+            existingGist.Public = Public;
+            existingGist.Title = Title;
+            existingGist.Language = Language;
+            existingGist.Description = Description;
+            existingGist.Content = Content;
+            return existingGist;
+        }
+
+        public static GistUpdateDTO FromModel(Gist model)
+        {
+            return new GistUpdateDTO
+            {
+                Id = model.Id,
+                Public = model.Public,
+                Title = model.Title,
+                Language = model.Language,
+                Description = model.Description,
+                Content = model.Content
+            };
+        }
+    }
+}
